310731bd790ec4691bdf2e4b8dc5fc939ff61be2
Penny <macphantomsoft@gmail.com>
2025-08-28 23:37:45 +0100

    master | feat: updating the rivy/ad-energy-card component

--- projects/rivy/src/app/components/ad-energy/ad-energy.component.ts
+++ projects/rivy/src/app/components/ad-energy/ad-energy.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { IonLabel } from "@ionic/angular/standalone";
+import { AdEnergyCardComponent } from "../cards-folder/ad-energy-card/ad-energy-card.component";
 
 @Component({
   selector: 'rivy-ad-energy',
   templateUrl: './ad-energy.component.html',
   styleUrls: ['./ad-energy.component.scss'],
   standalone: true,
-  imports: [IonLabel, CommonModule]
+  imports: [IonLabel, CommonModule, AdEnergyCardComponent]
 })
 export class AdEnergyComponent  implements OnInit {
 
